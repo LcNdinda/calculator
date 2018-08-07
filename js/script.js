8b43019cf1e54174749e3091eeed01fa17e70ac3
Lcndinda <lcndinda@gmail.com>
2018-08-07 09:02:01 +0300

    add a script file and a link to html

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var add = function(number1, number2){
+  return number1 + number2;
+};
+
+var num1 = prompt(parseInt("Enter a Number 1"));
+var num2 = prompt(parseInt("Enter a Number 2"));
+alert(num1, num2);
